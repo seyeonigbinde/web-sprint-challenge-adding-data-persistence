a2c7c8089ea2ca3b6ddb445cf6b5942e41d84b20
Your Name <you@example.com>
2021-06-12 00:21:05 +0100

    project model

--- api/project/model.js
+++ api/project/model.js
@@ -10,7 +10,7 @@ async function createProject(project) {
   const [project_id] = 
     await db('projects')
     .insert(project)
-    
+
   return getProjects()
     .where({ project_id })
     .first()
