1d95885f6d046047b31ce199fcfd207c2df137fc
Your Name <you@example.com>
2021-06-11 10:13:10 +0100

    index.js setup

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// start your server here
+const server = require('./api/server');
+
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
